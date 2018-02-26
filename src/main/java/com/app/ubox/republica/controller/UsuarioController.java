7402309831564944b008f462bfb1062b37ce21bd
jorge <jorge@widecommerce.biz>
2018-02-25 21:32:50 -0300

    Corrigindo pacote da classe UsuarioService

--- src/main/java/com/app/ubox/republica/controller/UsuarioController.java
+++ src/main/java/com/app/ubox/republica/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.ubox.republica.dto.UsuarioDto;
+import com.app.ubox.republica.service.UsuarioService;
 
 @RestController
 @RequestMapping("/user")
